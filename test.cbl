5143070c2134f98dd4dc557e44b9be0e26697326
Gutz <darckProyect8@gmail.com>
2024-06-16 19:25:59 -0500

    Comentarios

--- test.cbl
+++ test.cbl
@@ -1,9 +1,17 @@
+       identification division.
        PROGRAM-ID. TEST.
+       author GUTZ.
+       date-written "16/06/2024".
+       date-modified "16/06/2024".
+       date-compiled "16/06/2024".
+       installation "Empresa".
+       security ""
+       remarks "Este es un programa de prueba".
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
-            DISPLAY "String cualqu"
+            DISPLAY "Hola Mundo en Cobol"
             STOP RUN.
        END PROGRAM TEST.
