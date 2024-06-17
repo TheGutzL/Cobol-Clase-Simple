ca6a2207cf25cbadbd063f260dc2aef552195826
Gutz <darckProyect8@gmail.com>
2024-06-16 19:20:25 -0500

    Init

---
+++ test.cbl
@@ -0,0 +1,9 @@
+       PROGRAM-ID. TEST.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+            DISPLAY "String cualqu"
+            STOP RUN.
+       END PROGRAM TEST.
